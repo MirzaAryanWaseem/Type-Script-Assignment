f65f27c9ec03b7454b7f62d374b6c0aa38332ccf
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-04-25 22:56:32 +0500

    add q no 3

---
+++ Q No 3/main.js
@@ -0,0 +1,4 @@
+// QNo 3
+var personName = "babar azam";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
---
+++ Q No 3/main.ts
@@ -0,0 +1,4 @@
+// QNo 3
+let personName = "babar azam";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
