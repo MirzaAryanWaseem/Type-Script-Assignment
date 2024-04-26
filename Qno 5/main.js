395c63d7cfda54ac91f7293985730877d5565769
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-04-26 22:33:29 +0500

    Add Qno 5

---
+++ Qno 5/main.js
@@ -0,0 +1,4 @@
+//Q no 5
+var famousPerson = "Allama Muhammad Iqbal";
+var message = "".concat(famousPerson, " said, \"The ultimate aim of the ego is not to see something, but to be something\"");
+console.log(message);
---
+++ Qno 5/main.ts
@@ -0,0 +1,4 @@
+//Q no 5
+let famousPerson = "Allama Muhammad Iqbal";
+let message = `${famousPerson} said, "The ultimate aim of the ego is not to see something, but to be something"`;
+console.log(message);
