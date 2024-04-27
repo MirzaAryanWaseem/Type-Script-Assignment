e4d731366846d934f433d05c7af825b7c6ca48c0
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-04-27 22:25:27 +0500

    Add Q no7 &8

---
+++ Q no 7 & 8/main.js
@@ -0,0 +1,11 @@
+// Q no 7
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(32 / 4);
+// Q no 8
+console.log("Hello World");
+console.log(23 + 46);
+var userName = "Aryan";
+console.log("Hello from ".concat(userName, ",with using Type Script"));
+console.log(2 + 3);
---
+++ Q no 7 & 8/main.ts
@@ -0,0 +1,12 @@
+// Q no 7
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(32 / 4);
+
+// Q no 8
+console.log(`Hello World`);
+console.log(23 + 46);
+let userName = "Aryan";
+console.log(`Hello from ${userName},with using Type Script`);
+console.log(2 + 3);
