a215dc3d60370dda470555a2495d09fea470734e
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-04-28 22:31:46 +0500

    Add Q no 10

---
+++ Q no 10/main.js
@@ -0,0 +1,5 @@
+//This program print the person name.
+var personName = "Ali";
+console.log(personName);
+//This program print person message.
+console.log("Hello from ".concat(personName, ", with using type script."));
---
+++ Q no 10/main.ts
@@ -0,0 +1,6 @@
+//This program print the person name.
+let personName = "Ali";
+console.log(personName);
+//This program print person message.
+console.log(`Hello from ${personName}, with using type script.`);
+
