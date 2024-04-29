e0e71bb6518c68219b8d9fdcd77e927618ec6a0c
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-04-29 21:55:14 +0500

    Add Q no 6

---
+++ Q no 6/main.js
@@ -0,0 +1,5 @@
+//Q no 6
+var personName = "Babar Azam";
+console.log("\n\t".concat(personName, "\n\t")); // \t is used for spaces & \n is used for line spaces
+var user = "      azam khan    ";
+console.log("".concat(user.trim())); //.trim is used to remove the spaces
---
+++ Q no 6/main.ts
@@ -0,0 +1,22 @@
+//Q no 6
+let personName="Babar Azam"
+console.log(`\n\t${personName}\n\t`);// \t is used for spaces & \n is used for line spaces
+let user="      azam khan    "
+console.log(`${user.trim()}`);//.trim is used to remove the spaces
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
