0af10daba0e885d9e20bdd542e9e60a8d17de27d
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-04-30 22:31:18 +0500

    Add Q no11

---
+++ Q no 11/main.js
@@ -0,0 +1,6 @@
+//Q no 11
+var Name = ["Qasim", "Hammad", "Junaid", "Naveed"];
+console.log("".concat(Name[0]));
+console.log("".concat(Name[1]));
+console.log("".concat(Name[2]));
+console.log("".concat(Name[3]));
---
+++ Q no 11/main.ts
@@ -0,0 +1,8 @@
+//Q no 11
+
+let Name=["Qasim","Hammad","Junaid","Naveed"]
+console.log(`${Name[0]}`);
+console.log(`${Name[1]}`);
+console.log(`${Name[2]}`);
+console.log(`${Name[3]}`);
+
