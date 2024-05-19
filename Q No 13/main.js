9079fec295f4acbe602b6004f7d37d33f2314c81
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-05-19 13:00:04 +0500

    add q no 14

---
+++ Q No 13/main.js
@@ -0,0 +1,5 @@
+// // q13
+var Transport = ["Hayabusa", "Audi A6", "Civic"];
+console.log("my fav bike is ".concat(Transport[0]));
+console.log("my fav car is ".concat(Transport[1]));
+console.log("my other fav car is ".concat(Transport[2]));
---
+++ Q No 13/main.ts
@@ -0,0 +1,10 @@
+// // q13
+let Transport= ["Hayabusa","Audi A6","Civic"]
+
+console.log(`my fav bike is ${Transport[0]}`);
+console.log(`my fav car is ${Transport[1]}`);
+console.log(`my other fav car is ${Transport[2]}`);
+ 
+
+
+
