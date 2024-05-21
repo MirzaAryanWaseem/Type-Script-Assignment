e47795fadceca0ce0110c25fec7dc1acb781a1a9
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-05-21 18:52:29 +0500

    add q no 14

---
+++ Q No 14/main.js
@@ -0,0 +1,10 @@
+//Q no 14
+var party = ["Qasim", "Junaid", "Hammad", "Naveed"];
+for (var index = 0; index < party.length; index++) {
+    console.log("".concat(party[index], " you have invited to party."));
+}
+//Q no 15
+// let party2=["Qasim","Junaid","Hammad","Naveed"]
+// for (let index2 = 0; index2 < party2.length; index2++) {
+// console.log(`${party2[index2]} you have invited to party.`);
+// }
