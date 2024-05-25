f4e3801253fcd364cfaea2cd948ae9479caeacab
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-05-25 17:07:10 +0500

    edit

--- Q No 13/main.js
+++ Q No 13/main.js
@@ -3,3 +3,5 @@ var Transport = ["Hayabusa", "Audi A6", "Civic"];
 console.log("my fav bike is ".concat(Transport[0]));
 console.log("my fav car is ".concat(Transport[1]));
 console.log("my other fav car is ".concat(Transport[2]));
+//0,1,2 show array sequence 
+//which message you can print
