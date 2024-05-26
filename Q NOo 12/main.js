4c15b709abe752657250f5f7a5c230cd5c92db3b
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-05-26 19:07:29 +0500

    edit

--- Q NOo 12/main.js
+++ Q NOo 12/main.js
@@ -2,4 +2,5 @@
 var Name = ["Qasim", 'Junaid', "Naveed", "Hammad"];
 for (var index = 0; index < Name.length; index++) {
     console.log("".concat(Name[index], " is my friend. "));
-}
+} // for loop 
+//print message with all array 
--- Q NOo 12/main.ts
+++ Q NOo 12/main.ts
@@ -2,4 +2,5 @@
 let Name=["Qasim",'Junaid',"Naveed","Hammad"];
 for (let index = 0; index < Name.length; index++) {
    console.log(`${Name[index]} is my friend. `)   
-}
+}// for loop 
+//print message with all array 
