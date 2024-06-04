63eb30be82191d3f93d7b2f96c82d896fa531dc8
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-06-04 15:54:01 +0500

    edit

--- Q No 14/main.js
+++ Q No 14/main.js
@@ -1,6 +1,6 @@
 //Q no 14
 var party = ["Qasim", "Junaid", "Hammad", "Naveed"];
-for (var index = 1; index < party.length; index++) {
+for (var index = 2; index < party.length; index++) {
     console.log("".concat(party[index], " you have invited to party."));
 } //for loop
 // i=1 means starting from 1 value that is junaid in array
