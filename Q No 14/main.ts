a77b9a27c6e3d300acf9108d7de90af054cae6a2
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-06-05 17:36:03 +0500

    edit

--- Q No 14/main.ts
+++ Q No 14/main.ts
@@ -6,4 +6,5 @@ console.log(`${party[index]} you have invited to party.`);
     
 }//for loop
 // i=1 means starting from 1 value that is junaid in array
+// index < party length means break when array completed
 //Q no 15
