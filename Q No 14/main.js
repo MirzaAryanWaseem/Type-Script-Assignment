9f2a7c84f397f56f3c471515c69dc0f72ce8b7aa
MirzaAryanWaseem <160384342+MirzaAryanWaseem@users.noreply.github.com>
2024-06-14 18:43:44 +0500

    edit

--- Q No 14/main.js
+++ Q No 14/main.js
@@ -1,8 +1,15 @@
 //Q no 14
 var party = ["Qasim", "Junaid", "Hammad", "Naveed"];
-for (var index = 2; index < party.length; index++) {
+for (var index = 0; index < party.length; index++) {
     console.log("".concat(party[index], " you have invited to party."));
 } //for loop
 // i=1 means starting from 1 value that is junaid in array
 // index < party length means break when array completed
 //Q no 15
+var party2 = ["Qasim", "Junaid", "Hammad", "Naveed"];
+for (var index = 0; index < party.length; index++) {
+    console.log("".concat(party2[index], " you have invited to party."));
+    party2.pop();
+    party2.push("Mozammil");
+    console.log("".concat(party2[index], " you have invited to party."));
+}
--- Q No 14/main.ts
+++ Q No 14/main.ts
@@ -1,6 +1,6 @@
 //Q no 14
 let party=["Qasim","Junaid","Hammad","Naveed"]
-for (let index = 2; index < party.length ; index++) {
+for (let index = 0; index < party.length ; index++) {
 console.log(`${party[index]} you have invited to party.`);
 
     
@@ -8,3 +8,10 @@ console.log(`${party[index]} you have invited to party.`);
 // i=1 means starting from 1 value that is junaid in array
 // index < party length means break when array completed
 //Q no 15
+let party2=["Qasim","Junaid","Hammad","Naveed"]
+for (let index = 0; index < party.length ; index++) {
+console.log(`${party2[index]} you have invited to party.`);
+party2.pop();
+party2.push("Mozammil");
+console.log(`${party2[index]} you have invited to party.`);}
+
